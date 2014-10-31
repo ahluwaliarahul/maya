fda2f05fc994479bac71abc224ad8684aa062c3f
ahluwaliarahul <ahluwaliarahul@gmail.com>
2014-10-31 09:39:25 +0530

    Table 1

--- test.R
+++ test.R
@@ -1,2 +1,4 @@
 library(foreign)
 maya_transactions <- read.dta("C:/Documents and Settings/Ahlu/My Documents/IFMR/Credit Scoring/Mayavaram transactions.dta")
+length(unique(uniquegroup)) #Total number of schemes in transaction file
+table(noofticketstaken) #Number of members and tickets taken in transaction file
