0031383f61bb0d112237d6730aa6f66101113973
ahluwaliarahul <ahluwaliarahul@gmail.com>
2014-11-08 11:45:49 +0530

    Commit learn

--- members.R
+++ members.R
@@ -1,6 +1,32 @@
-#find number of members
-members<-data.frame(maya_transactions[c("name","fatherhusbandname","doorno")])
-to_remove <- which(members$name=="" & members$fatherhusbandname==""& members$doorno=="") #empty records
+#find individuals that are reported as separate members but are not
+members<-data.frame(maya_transactions)
+to_remove <- which(members$name_changed=="" & members$fatherhusbandname==""& members$doorno=="")  # empty records
 members<-members[-to_remove,]
-members$duplicates_name<-duplicated(members$name)
-members$dup_fname<-duplicated(members$fatherhusbandname)
+members$duplicates_name<-(duplicated(members$name_changed) | duplicated(members$name_changed, fromLast=TRUE))  # Mark duplicate names
+members$dup_fname<-(duplicated(members$fatherhusbandname) | duplicated(members$fatherhusbandname, fromLast=TRUE))  # Mark duplicate father names
+#members$dup_fname_not_name<- (!members$duplicates_name & members$dup_fname)  # Mark records with duplicate father names, but not duplicate names 
+#fnames_fname_dup<-members[members$dup_fname,2] #create vector with duplicate fathers names 
+reducedmem <- members[(members$dup_fname & !members$duplicates_name),]  # "create dataframe of members who have father names 
+                                                                  # in common with other members, but not their own name
+newmem<-reducedmem[order(reducedmem$fatherhusbandname),]
+members$dup_door <- (duplicated(members$doorno) | duplicated(members$doorno, fromLast=TRUE))  # duplicate door number
+#members$dup_door_not_name<- (!members$duplicates_name & members$dup_door)  # Mark records with duplicate doorno, but not duplicate names
+#doors_door_dup<-members[members$dup_door_not_name,3]
+reducedmem_door <- members[(members$dup_door & !members$duplicates_name),]
+newdoor<-reducedmem_door[order(reducedmem_door$doorno,reducedmem_door$name),]
+View(newdoor)
+View(newmem)
+reducedmem <- members[(members$duplicates_name),] # duplicate name 
+newmem_next<-reducedmem[order(reducedmem$name,reducedmem$fatherhusbandname),]
+reducedmem_door <- members[(!members$dup_door & members$duplicates_name),] # duplicate name but diff door
+newdoor_next<-reducedmem_door[order(reducedmem$name,reducedmem$doorno),]
+View(newdoor_next)
+View(newmem_next)
+rm(to_remove)
+rm(reducedmem)
+rm(reducedmem_door)
+rm(newdoor)
+rm(newmem)
+rm(members)
+rm(newdoor_next)
+rm(newmem_next)  
--- test.R
+++ test.R
@@ -1,4 +1,12 @@
 library(foreign)
-maya_transactions <- read.dta("C:/Documents and Settings/Ahlu/My Documents/IFMR/Credit Scoring/Mayavaram transactions.dta")
-length(unique(uniquegroup)) #Total number of schemes in transaction file
-table(noofticketstaken) #Number of members and tickets taken in transaction file
+maya_transactions <- read.dta("C:/Documents and Settings/Ahlu/My Documents/IFMR/Credit Scoring/Mayavaram transactionsv2.dta")
+var.labels <- attr(maya_transactions,"var.labels")
+data.key <- data.frame(var.name=names(maya_transactions),var.labels)
+length(unique(maya_transactions$uniquegroup)) #Total number of schemes in transaction file
+table(maya_transactions$noofticketstaken) #Number of tickets taken in transaction file
+length(unique(maya_transactions$name_changed)) #Total members
+range(maya_transactions$year_c,na.rm=T)  #starting point of time covered in dataset
+range(maya_transactions$year_t,na.rm=T)  #ending point of time covered
+range(maya_transactions$chitamount,na.rm=T) #Range of Chit Values
+range(maya_transactions$subsrs,na.rm=T) #Range of monthly contribution
+range(maya_transactions$monthduration,na.rm=T) #Range of scheme duration
